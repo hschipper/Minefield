4e91818da56c0d313958eddaf2f3929511713285
hschipper <hschipper@mail.csuchico.edu>
2016-09-26 11:51:08 -0700

    added some tests to FieldTest.cpp

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,30 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+
+/*TEST(FieldTest, getInBounds)
+{
+	Field minefield;
+	minefield.get(99,99);
+	ASSERT_EQ( "Out of bounds", minefield.get(99,99));
+
+}*/
+
+TEST(FieldTest, MineisSafe)
+{
+	Field minefield;
+        minefield.placeMine(4,5);
+        ASSERT_EQ( false, minefield.isSafe(4,5));
+}
+TEST(FieldTest, isSafe)
+{
+	Field minefield;
+	ASSERT_EQ( true, minefield.isSafe(5,5));
+
+}
+
+TEST(FieldTest, inBounds)
+{
+	Field minefield;
+	ASSERT_EQ( EMPTY_HIDDEN, minefield.get(5,5));
+}
